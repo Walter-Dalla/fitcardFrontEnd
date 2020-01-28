4a9c898a2d20f800eb87c7a29b95fb49a9296c75
walter-dalla <wwddttnn@hotmail.com>
2020-01-28 04:28:34 +0000

    Updates

---
+++ precache-manifest.898444c86751713573529d2218d9eeeb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df84e6beca1b2ac4730535d065c7825",
+    "url": "/fitcardFrontEnd/index.html"
+  },
+  {
+    "revision": "b8850994f293bda9ec10",
+    "url": "/fitcardFrontEnd/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "e1cafdb4b390cd308bf3",
+    "url": "/fitcardFrontEnd/static/css/main.df0c7d54.chunk.css"
+  },
+  {
+    "revision": "b8850994f293bda9ec10",
+    "url": "/fitcardFrontEnd/static/js/2.f92ec2d8.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/fitcardFrontEnd/static/js/2.f92ec2d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1cafdb4b390cd308bf3",
+    "url": "/fitcardFrontEnd/static/js/main.f199ed47.chunk.js"
+  },
+  {
+    "revision": "764f7018453354b4de47",
+    "url": "/fitcardFrontEnd/static/js/runtime-main.17bb945c.js"
+  },
+  {
+    "revision": "4a9c58aff569ae4e0e0f9ba1468a520c",
+    "url": "/fitcardFrontEnd/static/media/1.4a9c58af.jpg"
+  },
+  {
+    "revision": "0a115b73731ba8722ed060a5ff0a5012",
+    "url": "/fitcardFrontEnd/static/media/7.0a115b73.jpg"
+  },
+  {
+    "revision": "0a799148a50bb02c6f380eabd8d97559",
+    "url": "/fitcardFrontEnd/static/media/fontawesome-webfont.0a799148.svg"
+  },
+  {
+    "revision": "3293616ec0c605c7c2db25829a0a509e",
+    "url": "/fitcardFrontEnd/static/media/fontawesome-webfont.3293616e.woff"
+  },
+  {
+    "revision": "8b27bc96115c2d24350f0d09e6a9433f",
+    "url": "/fitcardFrontEnd/static/media/fontawesome-webfont.8b27bc96.eot"
+  },
+  {
+    "revision": "dcb26c7239d850266941e80370e207c1",
+    "url": "/fitcardFrontEnd/static/media/fontawesome-webfont.dcb26c72.ttf"
+  },
+  {
+    "revision": "68ed1dac06bf0409c18ae7bc62889170",
+    "url": "/fitcardFrontEnd/static/media/glyphicons-halflings-regular.68ed1dac.woff"
+  },
+  {
+    "revision": "7ad17c6085dee9a33787bac28fb23d46",
+    "url": "/fitcardFrontEnd/static/media/glyphicons-halflings-regular.7ad17c60.eot"
+  },
+  {
+    "revision": "e49d52e74b7689a0727def99da31f3eb",
+    "url": "/fitcardFrontEnd/static/media/glyphicons-halflings-regular.e49d52e7.ttf"
+  },
+  {
+    "revision": "ff423a4251cf2986555523dfe315c42b",
+    "url": "/fitcardFrontEnd/static/media/glyphicons-halflings-regular.ff423a42.svg"
+  }
+]);
